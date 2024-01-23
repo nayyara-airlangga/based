cc4cf8be07dfe57035a71d9e15822a0363dc8993
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-01-23 14:38:14 +0700

    feat: add let parsing

--- parser/parser.go
+++ parser/parser.go
@@ -27,5 +27,64 @@ func New(l *lexer.Lexer) *Parser {
 }
 
 func (p *Parser) Parse() *ast.Program {
-	return nil
+	program := &ast.Program{Statements: []ast.Statement{}}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curTok.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curTok}
+
+	// Expects an identifier after the let keyword
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curTok, Value: p.curTok.Literal}
+
+	// Expects an assign token after the identifier
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: parse the expressions
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curTok.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekTok.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
